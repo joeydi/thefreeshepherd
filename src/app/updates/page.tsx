9c160ab77a9bea1057dfa526228b23a1b0859274
Joe di Stefano <joeydi@okaypl.us>
2025-12-02 16:28:36 -0500

    add updates page

---
+++ src/app/updates/page.tsx
@@ -0,0 +1,9 @@
+export default async function Updates() {
+  return (
+    <main className="bg-[#282B2A] text-white">
+      <div className="bg-[#282B2A] relative z-1 h-screen flex items-center justify-center">
+        <h1 className="text-2xl">Latest updates</h1>
+      </div>
+    </main>
+  )
+}
