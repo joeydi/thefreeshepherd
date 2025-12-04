327fb2e0eb09effdb268343bc6d2fc83931b5593
Joe di Stefano <joeydi@okaypl.us>
2025-12-04 12:01:38 -0500

    add NewsletterSignup component

---
+++ components/newsletter-signup.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import backgroundImage from '@/images/newsletter-bg.jpg'
+import Image from 'next/image'
+import { useRef } from 'react'
+import { GlowingButton } from './glowing-button'
+
+export default function NewsletterSignup() {
+  const emailInput = useRef<HTMLInputElement>(null)
+
+  return (
+    <div className="rounded-fluid-xs relative mx-auto w-full max-w-6xl overflow-hidden bg-white/5 bg-linear-180 p-8 pt-[30vw] shadow-lg inset-ring-1 inset-ring-white/10 md:p-12 lg:p-16 xl:p-20">
+      <Image
+        className="absolute top-0 right-0 object-cover md:h-full md:w-full"
+        src={backgroundImage}
+        unoptimized
+        alt=""
+      />
+      <div className="rounded-fluid-xs absolute inset-0 hidden bg-[linear-gradient(to_right,#191C17cc,transparent_75%),linear-gradient(to_right,#191C17cc_25%,transparent_65%)] inset-ring inset-ring-white/10 md:block"></div>
+      <div className="relative flex max-w-xl flex-col gap-4">
+        <h2 className="font-serif text-2xl font-semibold tracking-tight text-white md:text-3xl md:text-balance lg:text-4xl">
+          Sign up for updates on our evolving world
+        </h2>
+        <p className="text-[#BCC8C6] md:text-balance">
+          Exclusive interviews, behind-the-scenes insights, and early glimpses into the story and
+          its faithful sheepdog guide.
+        </p>
+        <form
+          onSubmit={() => {
+            setTimeout(() => {
+              if (emailInput.current) {
+                emailInput.current.value = ''
+              }
+            })
+          }}
+          action="https://frameinteractive.us17.list-manage.com/subscribe/post?u=b9e140e94bbc0b1a691021dcd&amp;id=89bef8ceb9&amp;f_id=00507de0f0"
+          method="post"
+          id="mc-embedded-subscribe-form"
+          name="mc-embedded-subscribe-form"
+          className="mt-4 flex flex-col gap-4 md:flex-row md:items-end"
+          target="_blank"
+        >
+          <div className="min-w-[280px]">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="mce-EMAIL" className="font-serif font-light">
+                Email Address
+              </label>
+              <input
+                ref={emailInput}
+                type="email"
+                name="EMAIL"
+                className="rounded-sm bg-white/10 px-4 py-3 inset-ring-1 inset-ring-white/5"
+                id="mce-EMAIL"
+                required
+                defaultValue=""
+              />
+            </div>
+            <div aria-hidden="true" style={{ position: 'absolute', left: '-5000px' }}>
+              <input
+                type="text"
+                name="b_b9e140e94bbc0b1a691021dcd_89bef8ceb9"
+                tabIndex={-1}
+                defaultValue=""
+              />
+            </div>
+          </div>
+
+          <GlowingButton
+            type="submit"
+            name="subscribe"
+            id="mc-embedded-subscribe"
+            value="Subscribe"
+          >
+            Subscribe
+          </GlowingButton>
+        </form>
+      </div>
+    </div>
+  )
+}
