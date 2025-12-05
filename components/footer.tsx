7901a64cfba64caa20e27b43c6fec922812b46c0
Joe di Stefano <joeydi@okaypl.us>
2025-12-05 11:14:08 -0500

    mobile footer updates

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,22 +1,25 @@
+import Container from './container'
+
 export default function Footer() {
   return (
-    <footer className="relative z-2 flex flex-col justify-between gap-8 bg-[#282B2A] px-16 py-8 font-medium text-[#BCC8C6] md:flex-row">
-      <span className="lg:w-[35%]">
-        &copy; {new Date().getFullYear()}{' '}
-        <a href="https://frameinteractive.com/" target="_blank">
-          Frame Interactive Studio LLC
+    <footer>
+      <Container className="gap-2xs py-md flex flex-col items-center justify-between font-medium text-[#BCC8C6] md:flex-row">
+        <span className="lg:w-[35%]">
+          <a href="https://frameinteractive.com/" target="_blank">
+            &copy; {new Date().getFullYear()} Frame Interactive Studio LLC
+          </a>
+        </span>
+        <a
+          className="lg:w-[30%] lg:text-center"
+          href="https://frameinteractive.com/privacy"
+          target="_blank"
+        >
+          Privacy Policy
         </a>
-      </span>
-      <a
-        className="lg:w-[30%] lg:text-center"
-        href="https://frameinteractive.com/privacy"
-        target="_blank"
-      >
-        Privacy Policy
-      </a>
-      <a className="lg:w-[35%] lg:text-right" href="https://simplecreature.us" target="_blank">
-        Website by Simple Creature
-      </a>
+        <a className="lg:w-[35%] lg:text-right" href="https://simplecreature.us" target="_blank">
+          Website by Simple Creature
+        </a>
+      </Container>
     </footer>
   )
 }
